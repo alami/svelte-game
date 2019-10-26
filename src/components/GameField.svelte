cc95bdf205b3afaa16b6e5850def8d57be5f1a13
alami <alami@km.ru>
2019-10-26 16:11:16 +0500

    8. final

--- src/components/GameField.svelte
+++ src/components/GameField.svelte
@@ -3,8 +3,12 @@
     import Cannon from "./Cannon.svelte";
     // Импортируем компонент снаряда
     import Bullet from "./Bullet.svelte";
+    // импортируем компонент врагов
+    import Enemy from "./Enemy.svelte";
     // импортируем список снарядов из хранилища
     import { bulletList } from "../stores/cannon";
+    // импортируем список врагов из хранилища
+    import { enemyList } from "../stores/enemy";
 </script>
 <style>
     /* Сделаем так, чтобы наше игровое поле растягивалось на весь экран */
@@ -21,6 +25,10 @@
     <!-- Благодаря указанию атрибута viewBox пропорции нашего
            игрового поля будут сохраняться при изменении размеров -->
     <svg viewBox="0 0 480 800">
+        <!-- Добавим итерацию по нашему массиву врагов -->
+        {#each $enemyList as enemy (enemy.id)}
+            <Enemy {enemy} />
+        {/each}
         <!-- Добавим итерацию по нашему массиву снарядов -->
         {#each $bulletList as bullet (bullet.id)}
             <Bullet {bullet} />
--- src/gameLoop/gameLoop.js
+++ src/gameLoop/gameLoop.js
@@ -5,7 +5,10 @@ import { get } from 'svelte/store';
 // импортируем обработчик поворота пушки
 // Импортируем все обработчики событий пушки и снарядов
 import { rotateCannon, shoot, moveBullet, clearBullets } from "./cannon";
-
+// импортируем обработчик столкновений
+import { checkCollision } from './game';
+// Импортируем все обработчики событий врагов
+import { addEnemy, moveEnemy } from './enemy';
 
 // Функция отвечает за игровой цикл
 function startLoop(steps) {
@@ -26,9 +29,10 @@ export const startGame = () => {
     isPlaying.set(true);
     // запускаем игровой цикл. Пока массив шагов пустой
     // добавим обработчики в игровой цикл
-    startLoop([rotateCannon, shoot, moveBullet, clearBullets ]);
+    // добавим обработчики в игровой цикл
+    // добавим обработчик в игровой цикл
+    startLoop([rotateCannon, shoot, moveBullet, clearBullets, addEnemy, moveEnemy, checkCollision]);
 };
-
 // Функция отвечает за остановку игрового цикла
 export function stopGame() {
     // Устанавливаем переменную, которая хранит состояние игры в false
